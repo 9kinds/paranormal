0c28f0f4c1d309160e23e06163c3ba8750ea66e6
9kinds <shawn2275@gmail.com>
2016-03-30 16:38:43 -0500

    latest

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ angular.module("paranormalApp", [])
 	$scope.staffs = [
 		{
 			'name': "Peter Venkman, Ph.D.",
-			'picture': "img2/peter-venkman.jpg",
+			'picture': "img2/stantz.jpg",
 			'profile': "Dr. Venkman, who has doctorates in both psychology and parapsychology,"
 		},		{
 			'name': "Raymond Stantz, Ph.D.",
@@ -12,15 +12,15 @@ angular.module("paranormalApp", [])
 			'profile':"Dr. Stantz has been described as 'the heart of the Ghostbusters.' In his spare time he restores vintage cars and runs 'Ray's Occult Books.' "
 		},		{
 			'name': "Egon Spengler, Ph.D.",
-			'picture':"img2/egon.jpg",
+			'picture':"img2/stantz.jpg",
 			'profile':"Dr. Spengler is a former professor of paranormal studies at Columbia University. He has designed and built much of the firm's equipment. In his spare time he collects spores, molds, and fungus."
 		},		{
 			'name': "Winston Zeddemore",
-			'picture':"img2/winston.jpeg",
+			'picture':"img2/stantz.jpg",
 			'profile':"Mr. Zeddemore is a graduate of Air Force Command ECM school. He joined the firm shortly after its inception. In his spare time he is working on advancing his karate rank, currently a 1st Dan black belt."
 		},		{
 			'name': "Janine Melnitz",
-			'picture':"img2/janine.jpg",
+			'picture':"img2/stantz.jpg",
 			'profile':"Ms. Melnitz is the firm's receptionist and first point of contact for clients. In her spare time she's an avid reader. "
 		}
 	];
